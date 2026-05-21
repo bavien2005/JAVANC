6d44f109bf5797a04a3adcb98c16f24a1546b1db
Nguyễn Bá Viên <nguyenbavien.26092005@gmail.com>
2026-05-21 22:12:36 +0700

    D

--- hihi/BanTinh.cs
+++ hihi/BanTinh.cs
@@ -4,8 +4,10 @@ public class BanTinh : MonoBehaviour
 {
     [SerializeField] private float speed;
     private Vector3 dir;
+    private Rigidbody2D rb;
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         Destroy(gameObject, 3f);
     }
 
@@ -15,11 +17,21 @@ public class BanTinh : MonoBehaviour
         {
             return;
         }
-        transform.position += dir * speed * Time.deltaTime;
+        rb.linearVelocity = dir.normalized * speed;
     }
 
     public void SetDir(Vector3 dir)
     {
         this.dir = dir.normalized;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            print("asdasd");
+            Player1.instance.HoiMau();
+            Destroy(gameObject);
+        }
     }
 }
